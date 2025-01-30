f48b4c1a4858de112a3f20b24518a1314ef96dec
warhammer21 <shreyakvashisht21@gmail.com>
2025-01-30 13:30:27 -0800

    basic  consumer set up

--- order_placement.py
+++ order_placement.py
@@ -5,23 +5,31 @@ def order_placer(queue,order_list):
     for order in order_list:
         print(f"Producer: Producing {order}")
         queue.put(order)
-def producer_testing(queue, num_items):
-    """Producer that generates num_items and puts them into the queue."""
-    for i in range(num_items):
-        item = f"Task-{i}"
-        print(f"Producer: Producing {item}")
-        queue.put(item)
-        time.sleep(random.uniform(0.1, 0.5))  # Simulate variable production time
-    queue.put(None)
+def order_consumer(queue):
+    while True:
+        item = queue.get()
+        if item is None:  # Check for termination signal
+            print("Consumer: No more items to consume. Exiting.")
+            break
+        print(f"Consumer: Processing {item}")
 ## testng
 if __name__ == "__main__":
     print('Producer testing start')
-    num_items = 10
     order_list = [{'shoes':10},{'books':11},{'notepads':3}]
+    for _ in range(50):  # Generate 1000 entries
+        order_list.append({'shoes': random.randint(1, 4)})
+        order_list.append({'books': random.randint(1, 4)})
+        order_list.append({'notepads': random.randint(1, 4)})
     queue = multiprocessing.Queue()
     #producer_process = multiprocessing.Process(target=producer_testing, args=(queue, num_items))
     producer_process = multiprocessing.Process(target=order_placer, args=(queue,order_list))
+    consumer_process = multiprocessing.Process(target=order_consumer, args=(queue,))
+
     producer_process.start()
+    consumer_process.start()
+
     producer_process.join()
+    consumer_process.join()
+
 
 
